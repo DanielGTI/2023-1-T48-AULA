b1cc4554128f2266bd4544b9f7a2d2f8057fb5db
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2023-04-20 22:22:26 -0300

    Contagem do sorteio

--- Aula5/Form1.Designer.cs
+++ Aula5/Form1.Designer.cs
@@ -34,12 +34,17 @@
             txt_Sorteado_4 = new TextBox();
             txt_Sorteado_3 = new TextBox();
             txt_Sorteado_5 = new TextBox();
-            listBox1 = new ListBox();
+            lis_Numeros = new ListBox();
+            label1 = new Label();
+            lbl_nSorteio = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            list_TotalNumeros_1 = new ListBox();
+            list_TotalNumeros_2 = new ListBox();
             SuspendLayout();
             // 
             // txt_Sorteado_1
             // 
-            txt_Sorteado_1.Location = new Point(121, 32);
+            txt_Sorteado_1.Location = new Point(28, 95);
             txt_Sorteado_1.Name = "txt_Sorteado_1";
             txt_Sorteado_1.RightToLeft = RightToLeft.No;
             txt_Sorteado_1.Size = new Size(45, 23);
@@ -48,7 +53,7 @@
             // 
             // btn_Sorteio
             // 
-            btn_Sorteio.Location = new Point(121, 76);
+            btn_Sorteio.Location = new Point(28, 174);
             btn_Sorteio.Name = "btn_Sorteio";
             btn_Sorteio.Size = new Size(249, 23);
             btn_Sorteio.TabIndex = 1;
@@ -58,7 +63,7 @@
             // 
             // txt_Sorteado_2
             // 
-            txt_Sorteado_2.Location = new Point(172, 32);
+            txt_Sorteado_2.Location = new Point(79, 95);
             txt_Sorteado_2.Name = "txt_Sorteado_2";
             txt_Sorteado_2.RightToLeft = RightToLeft.No;
             txt_Sorteado_2.Size = new Size(45, 23);
@@ -67,7 +72,7 @@
             // 
             // txt_Sorteado_4
             // 
-            txt_Sorteado_4.Location = new Point(274, 32);
+            txt_Sorteado_4.Location = new Point(181, 95);
             txt_Sorteado_4.Name = "txt_Sorteado_4";
             txt_Sorteado_4.RightToLeft = RightToLeft.No;
             txt_Sorteado_4.Size = new Size(45, 23);
@@ -76,7 +81,7 @@
             // 
             // txt_Sorteado_3
             // 
-            txt_Sorteado_3.Location = new Point(223, 32);
+            txt_Sorteado_3.Location = new Point(130, 95);
             txt_Sorteado_3.Name = "txt_Sorteado_3";
             txt_Sorteado_3.RightToLeft = RightToLeft.No;
             txt_Sorteado_3.Size = new Size(45, 23);
@@ -85,28 +90,68 @@
             // 
             // txt_Sorteado_5
             // 
-            txt_Sorteado_5.Location = new Point(325, 32);
+            txt_Sorteado_5.Location = new Point(232, 95);
             txt_Sorteado_5.Name = "txt_Sorteado_5";
             txt_Sorteado_5.RightToLeft = RightToLeft.No;
             txt_Sorteado_5.Size = new Size(45, 23);
             txt_Sorteado_5.TabIndex = 4;
             txt_Sorteado_5.TextAlign = HorizontalAlignment.Center;
             // 
-            // listBox1
+            // lis_Numeros
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(121, 119);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(249, 124);
-            listBox1.TabIndex = 5;
+            lis_Numeros.FormattingEnabled = true;
+            lis_Numeros.ItemHeight = 15;
+            lis_Numeros.Location = new Point(28, 243);
+            lis_Numeros.Name = "lis_Numeros";
+            lis_Numeros.Size = new Size(249, 244);
+            lis_Numeros.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Sorteio:";
+            // 
+            // lbl_nSorteio
+            // 
+            lbl_nSorteio.AutoSize = true;
+            lbl_nSorteio.Location = new Point(81, 49);
+            lbl_nSorteio.Name = "lbl_nSorteio";
+            lbl_nSorteio.Size = new Size(13, 15);
+            lbl_nSorteio.TabIndex = 7;
+            lbl_nSorteio.Text = "0";
+            // 
+            // list_TotalNumeros_1
+            // 
+            list_TotalNumeros_1.FormattingEnabled = true;
+            list_TotalNumeros_1.ItemHeight = 15;
+            list_TotalNumeros_1.Location = new Point(321, 33);
+            list_TotalNumeros_1.Name = "list_TotalNumeros_1";
+            list_TotalNumeros_1.Size = new Size(111, 454);
+            list_TotalNumeros_1.TabIndex = 8;
+            // 
+            // list_TotalNumeros_2
+            // 
+            list_TotalNumeros_2.FormattingEnabled = true;
+            list_TotalNumeros_2.ItemHeight = 15;
+            list_TotalNumeros_2.Location = new Point(451, 33);
+            list_TotalNumeros_2.Name = "list_TotalNumeros_2";
+            list_TotalNumeros_2.Size = new Size(111, 454);
+            list_TotalNumeros_2.TabIndex = 9;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(486, 370);
-            Controls.Add(listBox1);
+            ClientSize = new Size(590, 493);
+            Controls.Add(list_TotalNumeros_2);
+            Controls.Add(list_TotalNumeros_1);
+            Controls.Add(lbl_nSorteio);
+            Controls.Add(label1);
+            Controls.Add(lis_Numeros);
             Controls.Add(txt_Sorteado_5);
             Controls.Add(txt_Sorteado_3);
             Controls.Add(txt_Sorteado_4);
@@ -115,6 +160,7 @@
             Controls.Add(txt_Sorteado_1);
             Name = "Form1";
             Text = "Sorteios";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -127,6 +173,11 @@
         private TextBox txt_Sorteado_4;
         private TextBox txt_Sorteado_3;
         private TextBox txt_Sorteado_5;
-        private ListBox listBox1;
+        private ListBox lis_Numeros;
+        private Label label1;
+        private Label lbl_nSorteio;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ListBox list_TotalNumeros_1;
+        private ListBox list_TotalNumeros_2;
     }
 }
--- Aula5/Form1.cs
+++ Aula5/Form1.cs
@@ -2,9 +2,16 @@ namespace Aula5
 {
     public partial class Form1 : Form
     {
+        public class Dados
+        {
+            //  Matriz de valores
+            public static int[,] valores =new int[61, 2];
+        }
+        
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void btn_Sorteio_Click(object sender, EventArgs e)
@@ -12,21 +19,30 @@ namespace Aula5
             int i, j, num, limite = 60;
             int[] sorteio = new int[5];
 
+            list_TotalNumeros_1.Items.Clear();
+            list_TotalNumeros_2.Items.Clear();
+
+
             Random aleatorio = new Random();
 
             for (i = 0; i < 5; i++)
             {
                 num = aleatorio.Next(limite) + 1;
                 sorteio[i] = num;
+                // CONTAR QUANTIDADE
+                Dados.valores[num, 0] += 1;
 
                 if (i > 0)
                     for (j = 0; j < i; j++)
                     {
                         if (sorteio[j] == sorteio[i])
                         {
+                            Dados.valores[sorteio[i], 0] -= 1; 
                             num = aleatorio.Next(limite) + 1;
                             sorteio[i] = num;
                             j = -1;
+                            // CONTAR QUANTIDADE
+                            Dados.valores[num, 0] += 1;
                         }
                     }
             }
@@ -43,7 +59,49 @@ namespace Aula5
             txt_Sorteado_5.Text = sorteio[4].ToString();
 
 
+            int numero_sorteio;
 
+            numero_sorteio = Convert.ToInt32(lbl_nSorteio.Text) + 1;
+            lbl_nSorteio.Text = numero_sorteio.ToString();
+
+            String linha_sorteio = "";
+            linha_sorteio += "Sorteio (";
+            linha_sorteio += numero_sorteio.ToString("00") + "):  ";
+            for (i = 0; i < 5; i++)
+            {
+                linha_sorteio += sorteio[i].ToString("00");
+                if (i < 4)
+                    linha_sorteio += " - ";
+            }
+            lis_Numeros.Items.Add(linha_sorteio);
+
+            string num_1;
+
+            for (i = 1; i <= 60; i++)
+            {
+                num_1 = i.ToString("00");
+                num_1 += ": ";
+                num_1 += Dados.valores[i, 0].ToString();
+
+                if( i <= 30 )
+                    list_TotalNumeros_1.Items.Add(num_1);
+                else
+                    list_TotalNumeros_2.Items.Add(num_1);
+
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i;
+
+            for (i = 1; i <= 60; i++)
+            {
+                Dados.valores[i, 0] = 0;
+            }
         }
     }
 }
